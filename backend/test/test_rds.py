c74a4c0a8eb48cfeca3340ac91e1c00bfe7ae85e
330i <id.kim.dev@proton.me>
2026-03-02 15:21:44 -0600

    RDS test function

---
+++ backend/test/test_rds.py
@@ -0,0 +1,28 @@
+import psycopg2
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+password = "837Owaq-sp3brlsplvq&hew*"
+
+conn = None
+try:
+    conn = psycopg2.connect(
+        DATABASE_URL,
+        sslmode='verify-full',
+    sslrootcert='global-bundle.pem'
+    )
+    cur = conn.cursor()
+    cur.execute('SELECT version();')
+    print(cur.fetchone()[0])
+    cur.close()
+except Exception as e:
+    print(f"Database error: {e}")
+    raise
+finally:
+    if conn:
+        conn.close()
